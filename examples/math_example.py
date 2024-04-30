1b401b511150b520a260c8fb170e995c204fb825
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-04-30 21:56:09 +0800

    feat: rename

--- examples/math_example.py
+++ examples/math_example.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append(".")
 
 from pymand import Pymand
+from math import sqrt
 
 
 def fib(num: str) -> int:
@@ -10,6 +11,16 @@ def fib(num: str) -> int:
     if num <= 2:
         return 1
     return fib(num - 1) + fib(num - 2)
+
+
+def is_prime(num: str) -> int:
+    num = int(num)
+    candidate_divisors = sqrt(num)
+    for divisor in range(2, int(candidate_divisors) + 1):
+        result = num / divisor
+        if result == int(result):
+            return False
+    return True
 
 
 def main():
